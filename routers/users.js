46414f021e994407b547bb42e55f9eeabd8d5a81
Shoaib Raza <hafizshoaibraza180@gmail.com>
2025-01-18 19:39:47 +0500

    Trainer ke Assignment or ClassWork ke graph add kr diye hein

--- routers/users.js
+++ routers/users.js
@@ -8,7 +8,7 @@ import Student from "../model/Student.js";
 const router = express.Router();
 
 const generateToken = (user) => {
-	return jwt.sign({ id: user._id, email: user.email, role: user.role }, "secretkey", {
+	return jwt.sign({ id: user._id, email: user.email, role: user.role }, process.env.JWT_SECRET, {
 		expiresIn: "1h",
 	});
 };
@@ -43,6 +43,7 @@ router.post("/login", async (req, res) => {
 		if (!isMatch) return res.status(401).json({ msg: "Invalid credentials" });
 
 		const token = generateToken(user);
+		console.log('token', token)
 		res.cookie("token", token, { httpOnly: true });
 
 		res.status(200).json({ msg: "Login successful", token, role: user.role, name: user.name, email: user.email, });
@@ -52,16 +53,39 @@ router.post("/login", async (req, res) => {
 });
 
 router.get("/profile", async (req, res) => {
+	
 	try {
-		console.log("Cookies: ", req.cookies); // Debug cookies received
+		// console.log("Cookies: ", req); // Debug cookies received
 
-		const token = req.cookies.token; // Check if token exists
-		if (!token) return res.status(401).json({ msg: "Unauthorized" });
+		// const token = req.cookies.token; // Check if token exists
+		// if (!token) return res.status(401).json({ msg: "Unauthorized" });
 
-		const decoded = jwt.verify(token, "secretkey");
-		const user = await User.findById(decoded.id).select("-password");
+		// const decoded = jwt.verify(token, "secretkey");
+		// const user = await User.findById(decoded.id).select("-password");
 
-		res.status(200).json(user);
+		const authHeader = req.headers.authorization; // Get the Authorization header
+
+		if (!authHeader || !authHeader.startsWith("Bearer ")) {
+		  return res.status(401).json({ msg: "Authorization token missing or invalid" });
+		}
+	  
+		const token = authHeader.split(" ")[1]; // Extract the token part (after "Bearer ")
+	  console.log('token in profile  ',token)
+		// Verify the token (using JWT or your chosen library)
+		console.log('jwtseceret', process.env.JWT_SECRET)
+		jwt.verify(token, process.env.JWT_SECRET, async(err, user) => {
+		  if (err) {console.log(err)
+			return res.status(403).json({ msg: "Invalid or expired token" });
+		  }
+	  
+		  // Token is valid, proceed with the request
+		  req.user = user; // Attach the decoded user data to the request object
+		  const singleUser = await User.findOne({email:user.email})
+		  console.log('singe user', singleUser)
+		  console.log('user in profile', user)
+		  res.status(200).json({ msg: "Token is valid", user:singleUser });
+		});
+	
 	} catch (error) {
 		console.error("Error: ", error.message);
 		res.status(500).json({ msg: "Error fetching user data", error: error.message });
